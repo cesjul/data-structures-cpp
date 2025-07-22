f39b50843f13327133506cc298f6aa6b4d98d22a
cesjul <juliocesarsouzacarvalho284@gmail.com>
2025-07-22 17:37:48 -0300

    Static arrays

---
+++ fundamentos/vetores/array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+const int ARRAY_LEN = 10;
+
+int main(){
+    int c[ARRAY_LEN];
+
+    c[0] = 10;
+    c[1] = 20;
+
+    for (int i = 0; i < ARRAY_LEN; i++){
+        std::cout << "c[" << i << "] = " << c[i] << std::endl;
+    }
+}
